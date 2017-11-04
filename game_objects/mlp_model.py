d35f8be9f86aa74584664720801258841d7455cd
DiegoAgher <diego.aguado@wizeline.com>
2017-11-04 14:49:05 -0600

    remove commented code

--- game_objects/mlp_model.py
+++ game_objects/mlp_model.py
@@ -14,15 +14,13 @@ class TensorFlowMLP(object):
 
             input_data = tf.placeholder(tf.float32, [None, (400 * 400)],
                                         name='input_data')
-            #with tf.variable_scope('hidden1'):
+
             w1 = tf.get_variable(shape=[400 * 400, 1000], name='w1')
             b1 = tf.get_variable(shape=[1000, ], name='b1')
 
-            #with tf.variable_scope('hidden2'):
             w2 = tf.get_variable(shape=[1000, 400], name='w2')
             b2 = tf.get_variable(shape=[400, ], name='b2')
 
-            #with tf.variable_scope('output'):
             w_output = tf.get_variable(shape=[400, 1], name='w_output')
             b_output = tf.get_variable(shape=[1, ],name='b_output')
 
@@ -44,7 +42,7 @@ class TensorFlowMLP(object):
 
         with tf.Session(graph=self.tf_graph) as sess:
             self.saver.restore(sess, "./tf_pong1.ckpt")
-            
+
             return sess.run(self.graph_elements['output'],
                             feed_dict=inference_feed_dict)
 
