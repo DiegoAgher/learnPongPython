ebac8a69f6b5f854f493648a3309a6690447f03f
DiegoAgher <diego.aguado@wizeline.com>
2017-11-08 14:22:20 -0600

    refactor class implementation to inherit from Player class

--- game_objects/TrainingPlayer.py
+++ game_objects/TrainingPlayer.py
@@ -3,23 +3,12 @@ import h5py
 import pygame
 from pygame.locals import *
 from datetime import datetime
+from game_objects.player import Player
 
 
-class TrainPlayer(pygame.sprite.Sprite):
+class TrainPlayer(Player):
     def __init__(self, name, screen, screen_height, screen_width):
-        pygame.sprite.Sprite.__init__(self)
-        self.SCR_HEI = screen_height
-        self.SCR_WID = screen_width
-        self.screen = screen
-        if name == "player1":
-            self.x, self.y = 16, self.SCR_HEI / 2
-        elif name == "player2":
-            self.x, self.y = self.SCR_WID - 16, self.SCR_HEI / 2
-        self.name = name
-        self.speed = 6
-        self.padWid, self.padHei = 8, 64
-        self.score = 0
-        self.scoreFont = pygame.font.SysFont("Arial", 32)
+        Player.__init__(self, name, screen, screen_height, screen_width)
 
     def scoring(self, screen_data):
         scoreBlit = self.scoreFont.render(str(self.score),
@@ -60,27 +49,3 @@ class TrainPlayer(pygame.sprite.Sprite):
 
                 data_file.close()
                 exit()
-
-    def movement(self):
-
-        keys = pygame.key.get_pressed()
-        if self.name == "player1":
-            if keys[pygame.K_w]:
-                self.y -= self.speed
-            elif keys[pygame.K_s]:
-                self.y += self.speed
-        elif self.name == "player2":
-            if keys[pygame.K_UP]:
-                self.y -= self.speed
-            elif keys[pygame.K_DOWN]:
-                self.y += self.speed
-
-        if self.y <= 0:
-            self.y = 0
-        elif self.y >= self.SCR_HEI - 64:
-            self.y = self.SCR_HEI - 64
-
-    def draw(self):
-        pygame.draw.rect(self.screen, (255, 255, 255),
-                         (self.x, self.y, self.padWid, self.padHei))
-
