b3645131497429530e89a1ccbcaa02488e0fe7fa
DiegoAgher <diego.aguado@wizeline.com>
2017-11-08 13:02:44 -0600

    refactor file with aiplayer classes

--- game_objects/AIPlayers.py
+++ game_objects/AIPlayers.py
@@ -4,7 +4,6 @@ from pygame.locals import *
 from sklearn.externals import joblib
 from keras.models import load_model
 from game_objects.player import Player
-from game_objects.mlp_model import MLP_model
 
 MODELS_PARAMETERS_DIR = 'pong_data/models_parameters/'
 x_mean_dir = MODELS_PARAMETERS_DIR + 'x_mean.pkl'
@@ -45,35 +44,6 @@ class ElasticNetPlayer(Player):
             prediction = model.predict(screen_np)
             self.y = prediction + self.y_mean
             print(self.y)
-
-
-class MLPlayer(Player):
-    def __init__(self, name, screen, screen_height, screen_width):
-        Player.__init__(self, name, screen, screen_height, screen_width)
-        self.predictive_model = load_model('pong_2_layers_mlp.h5')
-        self.y_mean = joblib.load('ymean.pkl')
-        self.x_mean = joblib.load('x_mean.pkl')
-
-    def scoring(self):
-        scoreBlit = self.scoreFont.render(str(self.score),
-                                          1, (255, 255, 255))
-        if self.name == "player1":
-            self.screen.blit(scoreBlit, (32, 16))
-            if self.score == 5:
-                print ("player 1 wins!")
-                exit()
-        elif self.name == "player2":
-            self.screen.blit(scoreBlit, (self.SCR_HEI + 92, 16))
-            if self.score == 5:
-                print ("Player 2 wins!")
-                exit()
-
-    def movement(self, screen_np):
-        if screen_np is not None:
-            model = self.predictive_model
-            prediction = model.predict(screen_np)
-            self.y = prediction + self.y_mean
-            print("predicted pos ",  self.y)
 
 
 class Conv2DPlayer(Player):
